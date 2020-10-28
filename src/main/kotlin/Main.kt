2c9eec67721f519bb17e7f3a14eaf91f6b2f88a0
timmyCoder <olatoyedan@gmail.com>
2020-10-28 23:09:14 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.application.log
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    val port = System.getenv("PORT")?.toInt() ?: 8080
+
+    val server = embeddedServer(Netty, port, module = Application::mainModule)
+        .start(wait = true)
+}
+
+fun Application.mainModule() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        trace {
+            application.log.debug(it.buildText())
+        }
+        get {
+            call.respond(mapOf("Welcome" to "FullStack Crud"))
+        }
+
+        get("/{name}") {
+            val name = call.parameters["name"]
+            call.respond(mapOf("username" to name))
+        }
+    }
+}
---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,39 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.ktor.application.Application
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun emptyPath() {
+        withTestApplication(Application::mainModule) {
+            val call = handleRequest(HttpMethod.Get, "")
+
+            assertEquals(
+                HttpStatusCode.OK, call.response.status()
+            )
+        }
+
+    }
+
+    @Test
+    fun checkValidValue() {
+        withTestApplication(Application::mainModule) {
+            val call = handleRequest(HttpMethod.Get, "/timmyCoder")
+            assertEquals(
+                """
+                {
+                  "username" : "timmyCoder"
+                }
+            """.asJson(), call.response.content?.asJson()
+            )
+        }
+    }
+}
+
+private fun String.asJson() = ObjectMapper().readTree(this)
